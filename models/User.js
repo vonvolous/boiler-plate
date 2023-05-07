b06aa9cb2e10f09460d187dbb21253e1b45d93dc
vonvolous <hanyoojin39@gmail.com>
2023-05-08 03:36:44 +0900

    login 과 auth 기능 구현

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name:{
@@ -53,6 +54,42 @@ userSchema.pre('save', function( next ) {
         next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword : 1234567 , 암호화된 비밀번호 : $2a$10$Q09NQ22QmPvzTLliJApUquVsPMpGKwPDkWJR7OdtwpDEMmt9QFbuq
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+
+    //jsonwebtoken 이용해서 트큰을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save().then(() => {
+        cb(null, user);
+    }).catch((err) => {
+        return cb(err);
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    //토큰 decoding
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용해 유저를 찾은 다음
+        //클라이언트에서 가져온 token과 db에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
