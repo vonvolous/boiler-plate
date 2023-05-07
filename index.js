0383f1c8bf398b0b372a50acb781c0ddd36df046
vonvolous <hanyoojin39@gmail.com>
2023-05-07 20:45:41 +0900

    add register in index.js

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const express = require('express')
 const app = express()
 // 특정 포트 백서버로 둠
 const port = 8080
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded를 client-> server로 분석해 가져옴
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json을 분석해 가져옴
+app.use(bodyParser.json());
 
 // mongoose 모듈 가져옴
 const mongoose = require('mongoose')
@@ -17,6 +25,24 @@ mongoose.connect('mongodb+srv://yoojin:yoojin@cluster0.06pbiyy.mongodb.net/?retr
 // root 디렉토리에 hello world 출력
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요')
+})
+
+//app.post(endpoint, callback function)
+app.post('/register', (req, res) => {
+    //회원 가입시 필요한 정보를 client에서 가져오면
+    //그것들을 db에 넣어준다.
+
+    // bodyParser이용해 정보를 받아 user에 저장
+    const user = new User(req.body)
+
+    //mongodb 메소드 user에 저장
+    user.save().then(() => {
+        res.status(200).json({
+            success: true
+        })
+    }).catch((err) => {
+        return res.json({ success: false, err })
+    })
 })
 
 // port에서 앱 실행
