b802c74c988157f02f0c91327ae7436a2c00f1da
vonvolous <hanyoojin39@gmail.com>
2023-05-08 01:46:25 +0900

    비밀번호 암호화 bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name:{
@@ -28,6 +30,27 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: { // 토큰 유효기간
         type: Number
+    }
+})
+
+
+// save하기 전에 하는 기능
+userSchema.pre('save', function( next ) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
     }
 })
 
