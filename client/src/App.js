24cfd08efe608e543504287cfd036a6ff16aa2e6
vonvolous <hanyoojin39@gmail.com>
2023-05-10 21:22:24 +0900

    react router dom

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  Routes, Route, Outlet, Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<LandingPage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
