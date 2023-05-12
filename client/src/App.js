7f69f5bb4a96584a857a2ad329b573246b199e79
vonvolous <hanyoojin39@gmail.com>
2023-05-13 01:58:47 +0900

    auth check

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,18 @@ import 'antd/dist/reset.css';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import Auth from './hoc/auth'
 
 function App() {
+  const AuthLandingPage = Auth(LandingPage, null);
+  const AuthLoginPage = Auth(LoginPage, false);
+  const AuthRegisterPage = Auth(RegisterPage, false);
+
   return (
     <Routes>
-      <Route exact path="/" element={<LandingPage />}></Route>
-      <Route path="/login" element={<LoginPage />}></Route>
-      <Route path="/register" element={<RegisterPage />}></Route>
+      <Route path="/" element={<AuthLandingPage />}></Route>
+      <Route path="/login" element={<AuthLoginPage />}></Route>
+      <Route path="/register" element={<AuthRegisterPage />}></Route>
     </Routes>
   );
 }
